599ed1e344a5125c5fc832e0d840e49e1f4eb273
Haodong Luo <42688847+LM233@users.noreply.github.com>
2022-02-17 17:36:43 -0500

    socrat version.

--- autoload.js
+++ autoload.js
@@ -1,6 +1,6 @@
 // 注意：live2d_path 参数应使用绝对路径
 const live2d_path =
-  "https://cdn.jsdelivr.net/gh/lm233/live2d-widget@latest/";
+  "https://cdn.jsdelivr.net/gh/lm233/live2d-widget@socrat/";
 //const live2d_path = "/live2d-widget/";
 
 // 封装异步加载资源的方法
